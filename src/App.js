9500667f8169be8780869665f4e9fdb2fea8e8e2
willonb <williamonb@gmail.com>
2024-05-15 18:40:59 -0300

    feat:react-source

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+
+function App() {
+  return (
+    <body>
+      <div className="card">
+        <h1>React Course</h1>
+        <p>Front-end Javascript</p>
+      </div>
+    </body>
+  );
+}
+
+export default App;
