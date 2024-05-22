314526154c9d0b84752b423ca8d06447c9b2d5b8
willonb <williamonb@gmail.com>
2024-05-21 22:13:35 -0300

    feat:atulização código

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import "./App.css";
 
+import Content from "./components/Team/components/content";
+import Section from "./components/Team/components/section";
 import Main from "./components/Team/components/main"
 
 function App() {
   return (
     <body>
+      
+      <div>
+        <Section />
+      </div>
 
-    <Main />
+      <div className="container-content">
+        <Content />
+        <Content />
+        <Content />
+      </div>
+      
+      <div className="container-content2">
+        <Content />
+        <Content />
+        <Content />
+      </div>
+
+      <div>
+        <Main />
+      </div>
 
     </body>
   );
