e481e9a46c2564e8a2c4128ba5f09470aecdb52f
willonb <williamonb@gmail.com>
2024-06-05 20:10:09 -0300

    fix:hook

---
+++ src/hooks/accordion/index.jsx
@@ -0,0 +1,13 @@
+import { AccotionButton, AccotionContainer, AccotionImage, AccotionText } from "./style";
+
+
+export default function accordion () {
+  return (
+   <AccotionContainer >
+     <AccotionText > </AccotionText>
+     <AccotionButton >
+       <AccotionImage ></AccotionImage>
+     </AccotionButton>
+   </AccotionContainer>
+  )
+}
---
+++ src/hooks/accordion/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+
+ export const AccotionContainer = styled.div`
+ width: 300px
+ border-bottom: 100%
+ height: 75px
+ display: flex;
+ padding-left: 20px;
+ align-items: center;
+ `
+
+ export const AccotionText = styled.p``
+ 
+ export const AccotionButton = styled.button``
+
+ export const AccotionImage = styled.image``
